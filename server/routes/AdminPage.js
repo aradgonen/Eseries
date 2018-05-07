0b84ba75283bc019ff7d021a1618f8685e169d96
Ort503Computer <gonena.arad@gmail.com>
2018-05-07 10:55:16 +0300

    cleaning

--- server/routes/AdminPage.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');//express module
-const router = express.Router();//express-router module
-const path = require('path');
-router.get('/Info', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/adminPage.html'));
-}); 
-module.exports = router;
